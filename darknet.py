bb2f73fd16c1860e12b66fffeb7c82236c335122
Krisha Bhambani <62170625+pixiedust18@users.noreply.github.com>
2020-06-19 19:34:12 +0530

    Update darknet.py

--- darknet.py
+++ darknet.py
@@ -492,7 +492,7 @@ def performDetect(imagePath="data/dog.jpg", thresh= 0.25, configPath = "./cfg/yo
                
             #---------------------------------------------
             print("img")
-            imgs = torch.from_numpy(np.asarray(result, dtype=np.float32))
+            imgs = torch.fromarray(result, dtype=np.float32))
             print("predic")
             predic = torch.from_numpy(np.asarray(predic, dtype=np.float32))
             comp = TensorDataset(imgs, predic)
